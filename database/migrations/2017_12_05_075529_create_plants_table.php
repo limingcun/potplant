cb343bcb24e6d2983a1ee836c2527292411e04cd
limingcun <1521010816@qq.com>
2017-12-11 17:22:10 +0800

    [MOD]修改迁移表

--- database/migrations/2017_12_05_075529_create_plants_table.php
+++ database/migrations/2017_12_05_075529_create_plants_table.php
@@ -16,9 +16,8 @@ class CreatePlantsTable extends Migration
         Schema::create('plants', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',50)->comment('盆栽品种名称');   //盆栽品种名称
-            $table->string('other_name',50)->nullable()->comment('别名');   //别名
-            $table->string('img',255)->nullable()->comment('品种图片');   //品种图片
-            $table->text('habit')->nullable()->comment('生长习性');   //生长习性
+            $table->text('img')->nullable()->comment('品种图片');   //品种图片
+            $table->text('intro')->nullable()->comment('介绍');   //介绍
             $table->text('memo')->nullable()->comment('其他信息');   //其他信息
             $table->softDeletes();   //deleted_at字段
             $table->timestamps();
--- database/migrations/2017_12_05_075534_create_plant_tabs_table.php
+++ database/migrations/2017_12_05_075534_create_plant_tabs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOperatesTable extends Migration
+class CreatePlantTabsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,15 @@ class CreateOperatesTable extends Migration
      */
     public function up()
     {
-        Schema::create('operates', function (Blueprint $table) {
+        Schema::create('plant_tabs', function (Blueprint $table) {
             $prefix = config('database.connections.prefix');
             $table->increments('id');
             $table->integer('plant_id')->unsigned()->comment('盆栽id');
-            $table->datetime('datetime')->comment('时间');
-            $table->text('info')->nullable()->comment('操作信息');
-            $table->text('img')->nullable()->comment('图片');
-            $table->string('type',255)->nullable()->comment('操作类型');
-            $table->text('memo')->nullable()->comment('其他信息');
+            $table->string('key')->nullable()->comment('键名');
+            $table->string('value')->nullable()->comment('键值');
             $table->softDeletes();   //deleted_at字段
             $table->timestamps();
-            $table->foreign('plant_id',$prefix.'operates_plant_id_foreign')->references('id')->on('plants');
+            $table->foreign('plant_id',$prefix.'plant_tabs_plant_id_foreign')->references('id')->on('plants');
         });
     }
 
@@ -35,6 +32,6 @@ class CreateOperatesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('operates');
+        Schema::dropIfExists('plant_tabs');
     }
 }
