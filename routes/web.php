e0213da8f2294a5adefb1f0423da5490e5b09bfa
limingcun <1521010816@qq.com>
2017-12-12 15:45:42 +0800

    [MOD]修改web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('get/openid','WebController@getOpenid'); //获取openid
 Route::group(['prefix' => 'home', 'namespace' => 'Home'], function() {
     // 前端路由
     Route::get('plant', 'IndexController@getPlant');
