bd116c283df4f556acd462c36232bc4cb792a6ce
limingcun <1521010816@qq.com>
2017-12-18 18:16:38 +0800

    [MOD]

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'admin/wxlogin',
-        'admin/plant'
+        'admin/wxlogin'
+//        'admin/plant'
     ];
 }
