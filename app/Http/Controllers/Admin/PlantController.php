5d2e50256e926b70c84051af43a47057c283de96
limingcun <1521010816@qq.com>
2017-12-18 14:56:59 +0800

    [MOD]修改PlantContorller.php

--- app/Http/Controllers/Admin/PlantController.php
+++ app/Http/Controllers/Admin/PlantController.php
@@ -14,8 +14,14 @@ class PlantController extends Controller
         $openid = IQuery::cleanInput($request->openid);
         $plant = Plant::join('plant_users','plants.id','=','plant_users.plant_id')
                 ->join('users','plant_users.user_id','=','users.id')
-                ->where('users.openid',$openid)
-                ->select('plants.*')->get();
+                ->where('users.openid',$openid);
+        if(isset($request->startDate)) {
+            $plant = where('plants.created_at','>=',$request->startDate);
+        } 
+        if(isset($request->endDate)) {
+            $plant = where('plants.created_at','<=',$request->endDate);
+        } 
+        $plant = $plant->select('plants.*')->get();
         return response()->json($plant);
     }
 }
