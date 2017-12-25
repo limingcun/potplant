5b4fcc5f64d085fd930b5fa54079349c6f412dba
limingcun <1521010816@qq.com>
2017-12-25 11:18:35 +0800

    [MOD]

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -44,7 +44,7 @@ class WebController extends Controller
         $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type='.$this->grant_type1.'&appid='.$this->appid.'&secret='.$this->secret;
         $res = file_get_contents($url);
         $res = json_encode($res);
-        return $res;
+        return $res['access_token'];
         $access_token = $res['access_token'];
         return $access_token;
         $data = array('path' => 'pages/test');
