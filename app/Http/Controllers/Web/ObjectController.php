0cf08f08609dbe369cc6d0260b433ef5c769685c
limingcun <1521010816@qq.com>
2018-01-03 11:36:14 +0800

    [MOD]修改ObjectController.php

--- app/Http/Controllers/Web/ObjectController.php
+++ app/Http/Controllers/Web/ObjectController.php
@@ -12,14 +12,21 @@ class ObjectController extends Controller
      * 邀请合作伙伴
      */
     public function getInvit(Request $request) {
-        $model = new PlantUser;
-        $arr = ['plant_id', 'user_id'];
-        $model->setRawAttributes($request->only($arr));
-        $model->type = 0;
-        if ($model->save()) {
-            return response()->json('true');
+        $plant_id = IQuery::cleanInput($request->plant_id);
+        $user_id = IQuery::cleanInput($request->user_id);
+        $plant_user = PlantUser::where('plant_id',$plant_id)->where('user_id',$user_id)->first();
+        if(!isset($plant_user)) {
+            $model = new PlantUser;
+            $arr = ['plant_id', 'user_id'];
+            $model->setRawAttributes($request->only($arr));
+            $model->type = 0;
+            if ($model->save()) {
+                return response()->json('true');
+            } else {
+                return response()->json('false');
+            }
         } else {
-            return response()->json('false');
+            return response()->json('true');
         }
     }   
 }
